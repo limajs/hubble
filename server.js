44efb0f91d8ebc5a491858a59ee07c7f8e064bf3
simoncrabtree <simon@crabsticks.net>
2015-11-22 15:24:38 +0000

    Added simple HapiJs hello world server

---
+++ server.js
@@ -0,0 +1,26 @@
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+
+var getHelloWorld = function (request, reply) {
+  return reply('hello world');
+};
+
+server.connection({ 
+  host: 'localhost', 
+  port: 8002 
+});
+
+
+server.route({
+  method: 'GET',
+  path:'/', 
+  handler: getHelloWorld 
+});
+
+server.start(function(err){
+  if (err) {
+    throw err;
+  }
+  console.log('Server running at:', server.info.uri);
+});
